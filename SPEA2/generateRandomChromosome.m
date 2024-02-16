a658b5c44571f4ac1f3ddb24344187a8f30b6c63
Ozan <ozankutlar@protonmail.com>
2024-02-16 10:37:42 +0300

    Changed to 2 genes as 3 gene mode doesnt work atm

--- SPEA2/generateRandomChromosome.m
+++ SPEA2/generateRandomChromosome.m
@@ -8,10 +8,10 @@ function [chrom] =  generateRandomChromosome()
     global op;  % optimization problem
     global gas;
     chrom = zeros(1,gas.numberOfDecisionVar);
-    %chrom(1,1) = rand;
-    %chrom(1,2) = rand * 3;
-    chrom(1,1) = (rand*10)-5;
-    chrom(1,2) = (rand*10)-5;
-    chrom(1,3) = (rand*10)-5;
+    chrom(1,1) = rand;
+    chrom(1,2) = rand * 3;
+    %chrom(1,1) = (rand*10)-5;
+    %chrom(1,2) = (rand*10)-5;
+    %chrom(1,3) = (rand*10)-5;
 end
 
--- SPEA2/runIt.m
+++ SPEA2/runIt.m
@@ -13,10 +13,10 @@ function [best_chrom, configurations] = runIt()
     gas.algorithm = "zdt1";
 
     % NOTE : Gene Bounds can be changed in generateRandomChromosome
-    gas.numberOfDecisionVar = 3;
+    gas.numberOfDecisionVar = 2;
     gas.generations = 100;
     gas.numberOfObjectives = 2;
-    gas.isMin = [1 1 1]; %vector for determining if the objectives are to minimize or maximize, 1 for minimize, 0 for maximize
+    gas.isMin = [1 1]; %vector for determining if the objectives are to minimize or maximize, 1 for minimize, 0 for maximize
     gas.onlyStrictlyDominance = false;
     gas.n_individuals = 300;
     gas.n_archive = gas.n_individuals;
