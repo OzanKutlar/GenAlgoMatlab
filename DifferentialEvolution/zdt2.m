259aa3b8a08ab9fc0dfdaaa5a479d6ab52351490
Kerem <kerembicen94@gmail.com>
2024-03-22 13:38:19 +0300

    Change in optimization problems of Differential Evolution

--- DifferentialEvolution/zdt2.m
+++ DifferentialEvolution/zdt2.m
@@ -1,7 +1,7 @@
-function f = zdt3(x)
+function f = zdt2(x)
     g = g1(x);
     f(1) = x(1);
-    f(2) = g * (1 - sqrt(x(1) / g) - x(1) / g * sin(10 * pi * x(1)));
+    f(2) = g * (1 - power(x(1) / g, 2));
 end
 
 function result = g1(x)
