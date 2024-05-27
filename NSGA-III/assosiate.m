5af326b5abe6762158aa6000f3f6fa5d9b4e9a1d
Ozan <ozankutlar@protonmail.com>
2024-05-27 14:11:28 +0300

    Added niching based new population into the code

--- NSGA-III/assosiate.m
+++ NSGA-III/assosiate.m
@@ -18,3 +18,37 @@ function [assosiations, reference_assosiations] = assosiate(normalizedfitArray, 
 end
 
 
+function distance = calculatePointToLineDistance(reference, point) 
+    % This function calculates the distance between a point and a line in n-dimensional space.
+    %
+    % Inputs:
+    % - lineEquation: A structure containing the following fields:
+    %   - point: A point on the line (vector)
+    %   - direction: The direction vector of the line (vector)
+    % - point: A vector representing the coordinates of the point
+    %
+    % Output:
+    % - distance: The shortest distance between the point and the line
+    
+    % Extract the point on the line and the direction vector from the line equation
+    pointOnLine = zeros(width(reference));
+    directionVector = reference;
+    
+    % Ensure the input point and line point have the same dimensions
+    if length(pointOnLine) ~= length(point) || length(directionVector) ~= length(point)
+        error('All points must have the same number of dimensions');
+    end
+    
+    % Calculate the vector from the point on the line to the given point
+    vectorToPoint = point - pointOnLine;
+    
+    % Project the vectorToPoint onto the direction vector
+    projectionLength = dot(vectorToPoint, directionVector) / norm(directionVector);
+    projectionVector = projectionLength * directionVector / norm(directionVector);
+    
+    % Calculate the orthogonal vector from the point to the line
+    orthogonalVector = vectorToPoint - projectionVector;
+    
+    % The distance is the norm of the orthogonal vector
+    distance = norm(orthogonalVector);
+end
--- NSGA-III/niching.m
+++ NSGA-III/niching.m
@@ -1,57 +1,35 @@
 function [newPop] = niching(fit_array, reference_assosiations, assosiations)
-    global g;
-    newPop = zeros(); 
-    smallestReference.count = reference_assosiations(1);
-    smallestReference.index = 1;
-    for i = 2:height(reference_assosiations)
-        if(smallestReference.count > reference_assosiations(i) && reference_assosiations(i) ~= 0)
-            smallestReference.count = reference_assosiations(i);
-            smallestReference.index = i;
+    global gas;
+    newPop = zeros(gas.n_individuals + 1, width(fit_array)); 
+    while true
+        smallestReference.count = reference_assosiations(1);
+        smallestReference.index = 1;
+        for i = 2:height(reference_assosiations)
+            if(smallestReference.count > reference_assosiations(i) && reference_assosiations(i) ~= 0)
+                smallestReference.count = reference_assosiations(i);
+                smallestReference.index = i;
+            end
+        end
+        
+        if(height(newPop) - (newPop(end, 1) + 1) >= smallestReference.count)
+            for i = 1:assosiations
+                if(assosiations(i).index == smallestReference.index)
+                    newPop(newPop(end, 1), :) = fit_array(i, :);
+                    newPop(end, 1) = newPop(end, 1) + 1;
+                end
+            end
+        end
+        for j = 1:height(newPop) - (newPop(end, 1) + 1)
+            nearestPoint.dist = inf;
+            for i = 1:height(assosiations)
+                if(assosiations(i).index == smallestReference.index && nearestPoint.dist > assosiations(i).dist)
+                    nearestPoint.dist = assosiations(i).dist;
+                    nearestPoint.index = i;
+                end
+            end
+            newPop(newPop(end, 1), :) = fit_array(nearestPoint.index, :);
         end
     end
+end
     
-    nearestPoint.dist = inf;
-    for i = 1:height(assosiations)
-        if(assosiations(i).index == smallestReference.index && nearestPoint.dist > assosiations(i).dist)
-            nearestPoint.dist = assosiations(i).dist;
-            nearestPoint.index = i;
-        end
-    end
 
-    newPop(end, :) = fit_array(nearestPoint.index, :);
-end
-    
-function distance = calculatePointToLineDistance(reference, point) 
-    % This function calculates the distance between a point and a line in n-dimensional space.
-    %
-    % Inputs:
-    % - lineEquation: A structure containing the following fields:
-    %   - point: A point on the line (vector)
-    %   - direction: The direction vector of the line (vector)
-    % - point: A vector representing the coordinates of the point
-    %
-    % Output:
-    % - distance: The shortest distance between the point and the line
-    
-    % Extract the point on the line and the direction vector from the line equation
-    pointOnLine = zeros(width(reference));
-    directionVector = reference;
-    
-    % Ensure the input point and line point have the same dimensions
-    if length(pointOnLine) ~= length(point) || length(directionVector) ~= length(point)
-        error('All points must have the same number of dimensions');
-    end
-    
-    % Calculate the vector from the point on the line to the given point
-    vectorToPoint = point - pointOnLine;
-    
-    % Project the vectorToPoint onto the direction vector
-    projectionLength = dot(vectorToPoint, directionVector) / norm(directionVector);
-    projectionVector = projectionLength * directionVector / norm(directionVector);
-    
-    % Calculate the orthogonal vector from the point to the line
-    orthogonalVector = vectorToPoint - projectionVector;
-    
-    % The distance is the norm of the orthogonal vector
-    distance = norm(orthogonalVector);
-end
